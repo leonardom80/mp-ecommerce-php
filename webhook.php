4eddf725d148fabec70f5cf4090220e8c94016ea
leonardom80 <31017096+leonardom80@users.noreply.github.com>
2021-05-04 12:34:11 -0500

    Update webhook.php
    insert Json in db

--- webhook.php
+++ webhook.php
@@ -1,17 +1,33 @@
 <?php
  //ini_set('display_errors',1);
  //error_reporting(E_ALL);
-echo __line__;
-//$data_json = json_decode(file_get_contents('php://input'), true);
-echo __line__;
-$fp = fopen("respuesta.txt", "w");
-echo __line__;
-fputs($fp, "pruebas");
-echo __line__;
-//$datos = json_encode($data_json , JSON_UNESCAPED_SLASHES );
-echo __line__;
-//fputs($fp, $datos);
-echo __line__;
-fclose($fp);
-echo __line__;
+            $servername = "ec2-18-209-98-235.compute-1.amazonaws.com";
+            $username = "amolina";
+            $password = "Master8098+";
+            $dbname = "tienda";
+
+            // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+
+            // Check connection
+            if ($conn->connect_error) {
+               die("Connection failed: " . $conn->connect_error);
+            }
+            
+	$data_json = json_decode(file_get_contents('php://input'), true);		
+    $datos = json_encode($data_json , JSON_UNESCAPED_SLASHES );
+
+			// Check connection
+            if ($conn->connect_error) {
+               die("Connection failed: " . $conn->connect_error);
+            } 
+            $sql = "INSERT INTO datos(json)VALUES ('".$datos."')";
+
+            if (mysqli_query($conn, $sql)) {
+               echo "Registro ingresado correctamente";
+            } else {
+               echo "Error: " . $sql . "" . mysqli_error($conn);
+            }
+            $conn->close();
+
 ?>
