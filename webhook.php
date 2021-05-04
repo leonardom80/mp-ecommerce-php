5072d15030b8ccb8704c9e927ac682daae35c9bc
leonardom80 <31017096+leonardom80@users.noreply.github.com>
2021-05-04 11:42:17 -0500

    debug

--- webhook.php
+++ webhook.php
@@ -1,16 +1,17 @@
 <?php
  //ini_set('display_errors',1);
  //error_reporting(E_ALL);
-
+echo __line__;
 $data_json = json_decode(file_get_contents('php://input'), true);
-
+echo __line__;
 $fp = fopen("respuesta.txt", "w");
+echo __line__;
 fputs($fp, "pruebas");
-
+echo __line__;
 $datos = json_encode($data_json , JSON_UNESCAPED_SLASHES );
-
+echo __line__;
 fputs($fp, $datos);
-
+echo __line__;
 fclose($fp);
-
+echo __line__;
 ?>
