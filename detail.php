0d8524ad76fe5462ba95b18c7e168253eae245da
leonardom80 <31017096+leonardom80@users.noreply.github.com>
2021-05-04 07:51:41 -0500

    Update detail.php

--- detail.php
+++ detail.php
@@ -1,6 +1,6 @@
 <?php
-// ini_set('display_errors',1);
-// error_reporting(E_ALL);
+ ini_set('display_errors',1);
+ error_reporting(E_ALL);
     // SDK de Mercado Pago
     require __DIR__ .  '/vendor/autoload.php';
     // Agrega credenciales
